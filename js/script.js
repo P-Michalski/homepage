b89b0e3335f8a23e94469fc17cebd2bde99ccb17
Piotr Michalski <pipi6620@gmail.com>
2023-04-25 21:41:17 +0200

    Changed code to base on functions

--- js/script.js
+++ js/script.js
@@ -1,24 +1,31 @@
-console.log("Witam wszystkich tu obecnych!");
+{
+    const onButtonClick = () => {
+        const body = document.querySelector(".js-body");
+        const header = document.querySelector(".js-header");
+        const mainHeading = document.querySelector(".js-headerHeading");
+        const themeIndicator = document.querySelector(".js-mainButtonTheme");
+        const sectionTexts = document.querySelectorAll(".js-sectionText");
+        const caption = document.querySelector(".js-tableCaption");
+        const footer = document.querySelector(".js-footer");
+        const footerText = document.querySelector(".js-footerText");
 
-let body = document.querySelector(".js-body");
-let header = document.querySelector(".js-header");
-let mainHeading = document.querySelector(".js-headerHeading");
-let button = document.querySelector(".js-mainButton");
-let themeIndicator = document.querySelector(".js-mainButtonTheme");
-let sectionTexts = document.querySelectorAll(".js-sectionText");
-let caption = document.querySelector(".js-tableCaption");
-let footer = document.querySelector(".js-footer");
-let footerText = document.querySelector(".js-footerText");
+        body.classList.toggle("body--dark");
+        header.classList.toggle("header--dark");
+        mainHeading.classList.toggle("header__heading--dark");
+        footer.classList.toggle("footer--dark");
+        sectionTexts.forEach((text) => {
+            text.classList.toggle("section__text--dark");
+        });
+        caption.classList.toggle("table__caption--dark");
+        footerText.classList.toggle("footer__text--dark");
+        themeIndicator.textContent === "ciemny" ? themeIndicator.textContent = "jasny" : themeIndicator.textContent = "ciemny";
+    };
 
-button.addEventListener("click", () => {
-    body.classList.toggle("body--dark");
-    header.classList.toggle("header--dark");
-    mainHeading.classList.toggle("header__heading--dark");
-    footer.classList.toggle("footer--dark");
-    sectionTexts.forEach((text) => {
-        text.classList.toggle("section__text--dark");
-    });
-    caption.classList.toggle("table__caption--dark");
-    footerText.classList.toggle("footer__text--dark");
-    themeIndicator.textContent === "ciemny" ? themeIndicator.textContent = "jasny" : themeIndicator.textContent = "ciemny";
-});
+    const init = () => {
+        console.log("Witam wszystkich tu obecnych!");
+
+        const button = document.querySelector(".js-mainButton");
+        button.addEventListener("click", onButtonClick);
+    };
+    init()
+}
